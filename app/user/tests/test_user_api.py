7ce8adcd3d8803e789e9cb3778d93f29392c4a9e
Jong Hur <jonghur@jongs-mbp-2.lan>
2021-08-28 21:29:08 -0400

    ADded manage user endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse("user:token")
+ME_URL = reverse("user:me")
 
 
 def create_user(**param):
@@ -54,17 +55,14 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(
-            email=payload["email"]
-        ).exists()
+        user_exists = (
+            get_user_model().objects.filter(email=payload["email"]).exists()
+        )
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
         """Test that a token is created for the user"""
-        payload = {
-            "email": "test@appdev.com",
-            "password": "testpass"
-        }
+        payload = {"email": "test@appdev.com", "password": "testpass"}
         create_user(**payload)
         res = self.client.post(TOKEN_URL, payload)
 
@@ -74,10 +72,7 @@ class PublicUserApiTests(TestCase):
     def test_token_invalid_credentials(self):
         """Test that token is not created if invalid credentials are given"""
         create_user(email="test@appdev.com", password="testpass")
-        payload = {
-            "email": "test@appdev.com",
-            "password": "badpass"
-        }
+        payload = {"email": "test@appdev.com", "password": "badpass"}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn("token", res.data)
@@ -85,10 +80,7 @@ class PublicUserApiTests(TestCase):
 
     def test_create_token_no_user(self):
         """Test that no usder is created if token does not exist"""
-        payload = {
-            "email": "test@appdev.com",
-            "password": "badpass"
-        }
+        payload = {"email": "test@appdev.com", "password": "badpass"}
         res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -98,3 +90,50 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, {"email": "one", "password": ""})
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUsersApiTest(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email="test@appdev.com",
+            password="testpass",
+            name="name",
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data, {"name": self.user.name, "email": self.user.email}
+        )
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_user_update_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {
+            "name": "new name",
+            "password": "newpassword123",
+        }
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload["name"])
+        self.assertTrue(self.user.check_password(payload["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
